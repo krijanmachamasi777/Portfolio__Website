c0077525ad720f3206b09ed026531b8e2b1c11d7
krijanmachamasi777 <krijanmachamasi37@gmail.com>
2025-08-08 00:35:35 +0545

    udated latest projects

--- src/Components/MyWork/MyWork.jsx
+++ src/Components/MyWork/MyWork.jsx
@@ -7,20 +7,19 @@ import arrow_icon from '../../assets/arrow_icon.svg'
 const MyWork = () => {
   return (
     <div id="work" className='mywork'>
-        <div className="mywork-title">
+      <div className="mywork-title">
         <h1>My Latest Work</h1>
         <img src={theme_pattern} alt="" />
       </div>
       <div className="mywork-container">
-        {mywork_data.map((work,index)=>
-        {
-            return <img key={index} src={work.w_img} alt=""/>
+        {mywork_data.map((work, index) => {
+          return <a href={work.link} key={index}><img src={work.w_img} alt="" /></a>
         })}
-        </div>
-        <div className="mywork-showmore">
-            <p>Show More</p>
-            <img src={arrow_icon} alt="" />
-        </div>
+      </div>
+      <div className="mywork-showmore">
+        <p>Show More</p>
+        <img src={arrow_icon} alt="" />
+      </div>
     </div>
   )
 }
--- src/assets/mywork_data.js
+++ src/assets/mywork_data.js
@@ -4,17 +4,21 @@ import project3_img from '../assets/project_3.svg'
 import project4_img from '../assets/project_4.svg'
 import project5_img from '../assets/project_5.svg'
 import project6_img from '../assets/project_6.svg'
+import Netflix_img from '../assets/Netflix.png'
+import Todo_list from '../assets/Todo_list.png'
 
 const mywork_data = [
     {
         w_no:1,
         w_name:"Web design",
-        w_img:project1_img
+        w_img:Netflix_img,
+        link:"https://movie-ui-clone-git-main-krijanmachamasi777s-projects.vercel.app?_vercel_share=yqnxDjqkBR3sUaER5xdjbnyxQRrLpsqP"
     },
     {
         w_no:2,
         w_name:"Web design",
-        w_img:project2_img
+        w_img:Todo_list,
+        link:"https://todo-list-git-main-krijanmachamasi777s-projects.vercel.app?_vercel_share=MVXluYlRA3rAkGAUGl3BCK6pegjl7WSb"
     },
     {
         w_no:3,
