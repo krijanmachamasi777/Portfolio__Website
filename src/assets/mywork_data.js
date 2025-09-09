0f75189a49f23e00e6d5235320f6026d06e53bcd
krijanmachamasi777 <krijanmachamasi37@gmail.com>
2025-09-09 20:44:40 +0545

    project maintained

--- src/assets/mywork_data.js
+++ src/assets/mywork_data.js
@@ -1,46 +1,48 @@
-import Netflix_img from '../assets/Netflix.png'
-import Todo_list from '../assets/Todo_list.png'
-import Basic_project from '../assets/Basic_project.png'
-import Demo_website from '../assets/Demo_website.png'
-import Weather_App from '../assets/Weather_App.png'
-import project6_img from '../assets/project_6.svg'
+import Netflix_img from "../assets/Netflix.png";
+import Todo_list from "../assets/Todo_list.png";
+import Basic_project from "../assets/Basic_project.png";
+import Demo_website from "../assets/Demo_website.png";
+import Weather_App from "../assets/Weather_App.png";
+import Rockpaer from "../assets/Rockpaper.png";
 
 const mywork_data = [
-    {
-        w_no:1,
-        w_name:"Web design",
-        w_img:Netflix_img,
-        link:"https://movie-ui-clone-git-main-krijanmachamasi777s-projects.vercel.app?_vercel_share=xK1csMix88qhiOaFTxUqNIwxRXYoM5ye"
-    },
-    {
-        w_no:2,
-        w_name:"Web design",
-        w_img:Todo_list,
-        link:"https://todo-list-git-main-krijanmachamasi777s-projects.vercel.app?_vercel_share=MVXluYlRA3rAkGAUGl3BCK6pegjl7WSb"
-    },
-    {
-        w_no:3,
-        w_name:"Web design",
-        w_img:Basic_project,
-        link:"https://basic-html-css-git-main-krijanmachamasi777s-projects.vercel.app?_vercel_share=lqdIEOLw38HNnetC3eCdOGymqUWVWgTK"
-    },
-    {
-        w_no:4,
-        w_name:"Web design",
-        w_img:Demo_website,
-        link:"https://demo-website-git-main-krijanmachamasi777s-projects.vercel.app?_vercel_share=oGCcV0OgI85hRzQp41tkLBp7HZYMeo7d"
-    },
-    {
-        w_no:5,
-        w_name:"Web design",
-        w_img:Weather_App,
-        link:"https://weather-app-git-main-krijanmachamasi777s-projects.vercel.app?_vercel_share=CSpBZBy6dSJEvXytXrcD6coQa1OCQybH"
-    },
-    {
-        w_no:6,
-        w_name:"Web design",
-        w_img:project6_img
-    },
-]
- 
+  {
+    w_no: 1,
+    w_name: "Web design",
+    w_img: Netflix_img,
+    link: "https://movie-ui-clone-git-main-krijanmachamasi777s-projects.vercel.app?_vercel_share=xK1csMix88qhiOaFTxUqNIwxRXYoM5ye",
+  },
+  {
+    w_no: 2,
+    w_name: "Web design",
+    w_img: Weather_App,
+    link: "https://weather-app-git-main-krijanmachamasi777s-projects.vercel.app?_vercel_share=CSpBZBy6dSJEvXytXrcD6coQa1OCQybH",
+  },
+  {
+    w_no: 3,
+    w_name: "Web design",
+    w_img: Rockpaer,
+    link: "https://rock-paper-scissor-git-main-krijanmachamasi777s-projects.vercel.app?_vercel_share=5OcYWZVVSA1Kcfupzv0sOFgh1ByJIosp",
+  },
+
+  {
+    w_no: 4,
+    w_name: "Web design",
+    w_img: Basic_project,
+    link: "https://basic-html-css-git-main-krijanmachamasi777s-projects.vercel.app?_vercel_share=lqdIEOLw38HNnetC3eCdOGymqUWVWgTK",
+  },
+  {
+    w_no: 5,
+    w_name: "Web design",
+    w_img: Todo_list,
+    link: "https://todo-list-git-main-krijanmachamasi777s-projects.vercel.app?_vercel_share=MVXluYlRA3rAkGAUGl3BCK6pegjl7WSb",
+  },
+  {
+    w_no: 6,
+    w_name: "Web design",
+    w_img: Demo_website,
+    link: "https://demo-website-git-main-krijanmachamasi777s-projects.vercel.app?_vercel_share=oGCcV0OgI85hRzQp41tkLBp7HZYMeo7d",
+  },
+];
+
 export default mywork_data;
