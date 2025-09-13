52d0af89fb1b9dc35f606aa7ea73e38f03953d9e
krijanmachamasi777 <krijanmachamasi37@gmail.com>
2025-09-13 17:38:26 +0545

    project added

--- src/assets/mywork_data.js
+++ src/assets/mywork_data.js
@@ -4,6 +4,7 @@ import Basic_project from "../assets/Basic_project.png";
 import Demo_website from "../assets/Demo_website.png";
 import Weather_App from "../assets/Weather_App.png";
 import Rockpaer from "../assets/Rockpaper.png";
+import E_Commerce_website from "../assets/Ecommerce.png";
 
 const mywork_data = [
   {
@@ -12,37 +13,39 @@ const mywork_data = [
     w_img: Netflix_img,
     link: "https://movie-ui-clone-git-main-krijanmachamasi777s-projects.vercel.app?_vercel_share=xK1csMix88qhiOaFTxUqNIwxRXYoM5ye",
   },
-  {
+   {
     w_no: 2,
+    w_name: "Web design",
+    w_img: E_Commerce_website,
+    link: "https://e-commerce-website-git-main-krijanmachamasi777s-projects.vercel.app?_vercel_share=V1v6rbaBLyqi7nrcTNHg1jwyOXcbx05V",
+  },
+  {
+    w_no: 3,
     w_name: "Web design",
     w_img: Weather_App,
     link: "https://weather-app-git-main-krijanmachamasi777s-projects.vercel.app?_vercel_share=CSpBZBy6dSJEvXytXrcD6coQa1OCQybH",
   },
   {
-    w_no: 3,
+    w_no: 4,
     w_name: "Web design",
     w_img: Rockpaer,
     link: "https://rock-paper-scissor-git-main-krijanmachamasi777s-projects.vercel.app?_vercel_share=5OcYWZVVSA1Kcfupzv0sOFgh1ByJIosp",
   },
+  
 
   {
-    w_no: 4,
+    w_no: 5,
     w_name: "Web design",
     w_img: Basic_project,
     link: "https://basic-html-css-git-main-krijanmachamasi777s-projects.vercel.app?_vercel_share=lqdIEOLw38HNnetC3eCdOGymqUWVWgTK",
   },
   {
-    w_no: 5,
+    w_no: 6,
     w_name: "Web design",
     w_img: Todo_list,
     link: "https://todo-list-git-main-krijanmachamasi777s-projects.vercel.app?_vercel_share=MVXluYlRA3rAkGAUGl3BCK6pegjl7WSb",
   },
-  {
-    w_no: 6,
-    w_name: "Web design",
-    w_img: Demo_website,
-    link: "https://demo-website-git-main-krijanmachamasi777s-projects.vercel.app?_vercel_share=oGCcV0OgI85hRzQp41tkLBp7HZYMeo7d",
-  },
+ 
 ];
 
 export default mywork_data;
