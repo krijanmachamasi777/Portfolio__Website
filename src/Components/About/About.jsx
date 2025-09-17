0afea204732a1bab4693e38a9fbd575766e1b6d0
krijanmachamasi777 <krijanmachamasi37@gmail.com>
2025-09-17 16:36:53 +0545

    image link changed

--- src/Components/About/About.jsx
+++ src/Components/About/About.jsx
@@ -11,7 +11,7 @@ const About = () => {
             </div>
             <div className="about-sections">
                 <div className="about-left">
-                    <img src="https://ik.imagekit.io/bnpgsmexh/Protfolio%20projects/mypic.jpg?updatedAt=1758105448803" alt="" />
+<img src="https://ik.imagekit.io/bnpgsmexh/Protfolio%20projects/mypic.jpg?tr=w-500,q-80,f-auto" alt="profile" />
                 </div>
 
                 <div className="about-right">
