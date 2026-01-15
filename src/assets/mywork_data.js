6d7c761e6c9fdac47088eb5f9cf747f56d80bca7
krijanmachamasi777 <krijanmachamasi37@gmail.com>
2026-01-15 08:29:28 +0545

    new Food_delivery_website project added

--- src/assets/mywork_data.js
+++ src/assets/mywork_data.js
@@ -5,49 +5,50 @@ import Demo_website from "../assets/Demo_website.png";
 import Weather_App from "../assets/Weather_App.png";
 import Rockpaer from "../assets/Rockpaper.png";
 import E_Commerce_website from "../assets/Ecommerce.png";
+import Food_Delivery_Website from "../assets/Food_Delivery_Website.png";
 
 const mywork_data = [
   {
     w_no: 1,
+    title: "Food Delivery Website",
+    desc: "A full-stack food delivery application with eSewa payment gateway integration and a modified admin panel for managing products, orders, and users.",
+    w_img: Food_Delivery_Website,
+    link: "https://food-delivery-website-git-main-krijanmachamasi777s-projects.vercel.app?_vercel_share=5tT4DyDB0NEbaFK77DVeaJ1YwpqEwY17",
+  },
+  {
+    w_no: 2,
     title: "Netflix UI Clone",
-    desc: "A Netflix homepage clone built with React JS, focusing on layout, styling, responsive design. Here I used firebase for authentication.",
+    desc: "A Netflix homepage clone built with React JS, focusing on layout, styling, and responsive design. Firebase is used for authentication.",
     w_img: Netflix_img,
     link: "https://movie-ui-clone-rosy.vercel.app/",
   },
   {
-    w_no: 2,
+    w_no: 3,
     title: "E-Commerce Website",
     desc: "An online store built with React and Ant Design. Includes product listing, shopping cart, and checkout workflow.",
     w_img: E_Commerce_website,
     link: "https://e-commerce-website-two-coral.vercel.app/",
   },
   {
-    w_no: 3,
+    w_no: 4,
     title: "Weather App",
-    desc: "A weather forecast application using React and OpenWeather API to display live weather data with responsive UI.",
+    desc: "A weather forecast application using React and OpenWeather API to display live weather data with a responsive UI.",
     w_img: Weather_App,
     link: "https://weather-app-six-khaki-17.vercel.app/",
   },
   {
-    w_no: 4,
+    w_no: 5,
     title: "Rock Paper Scissors Game",
     desc: "A simple game built with React and JavaScript logic to practice state management and event handling.",
     w_img: Rockpaer,
     link: "https://rock-paper-scissor-snowy-eta.vercel.app/",
   },
   {
-    w_no: 5,
+    w_no: 6,
     title: "Basic HTML & CSS Project",
     desc: "A beginner project created with pure HTML and CSS to strengthen fundamentals of layout and styling.",
     w_img: Basic_project,
     link: "https://basic-html-css.vercel.app/",
-  },
-  {
-    w_no: 6,
-    title: "To-Do List App",
-    desc: "A task manager built with React that allows adding, deleting, and managing tasks with local state.",
-    w_img: Todo_list,
-    link: "https://todo-list-six-kappa-15.vercel.app/",
   },
 ];
 
